b5baa29171eb3ad950db14b2d8a70509c141f2bc
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-06-27 02:04:04 +0200

    Quant KV and Draft QKV, 24 modes
    With customizable QKV for the draft as well.
    And reduced Blas Batch Size for the draft model.

--- expose.h
+++ expose.h
@@ -70,6 +70,8 @@ struct load_model_inputs
     const float tensor_split[tensor_split_max] = {};
     const int quant_k = 0;
     const int quant_v = 0;
+    const int draft_quant_k = -1;
+    const int draft_quant_v = -1;
     const bool check_slowness = false;
     const bool highpriority = false;
     const bool swa_support = false;
