9e3a60d765e67e8d70a108a44d5989b2710faec5
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-07-16 20:04:02 +0200

    iq1_kt: basics
    iq1_kt: CUDA dequantize
    
    Testing with LlaMA-3.1-8B-Instruct, we get almost the same PPL
    as iq2_xxs, so about 0.2 bpw fewer bits for the same quality.
    
    iq1_kt: CUDA MMQ
    
    iq1_kt: CUDA MMVQ
    
    iq1_kt: AVX2 GEMM/GEMV
    
    iq1_kt: convert/repack to q8_0_r8 (AVX2)
    
    iq1_kt: slightly faster GEMV
    
    18.6 t/s -> 19.4 t/s
    
    iq1_kt: NEON GEMM/GEMV
    
    Pathetic as usual
    
    iq1_kt: slightly faster NEON - still pathetic
    
    iq1_kt: tiny bit better GEMV on NEON
    
    iq1_kt: convert/repack to q8_0_r8 (NEON)
    
    iq1_kt: very slightly faster convert/repack to q8_0_r8 on NEON
    
    Adding frgotten file
    
    Update stable-diffusion.h
    
    Update IKL files, including IQ1_KT
    
    Author : Ikawrakow

--- ggml/src/ggml-cuda/mmq.cu
+++ ggml/src/ggml-cuda/mmq.cu
@@ -69,6 +69,39 @@ static void ggml_cuda_mul_mat_q_switch_type(ggml_backend_cuda_context & ctx, con
         case GGML_TYPE_IQ4_NL:
             mul_mat_q_case<GGML_TYPE_IQ4_NL>(ctx, args, stream);
             break;
+        case GGML_TYPE_IQ2_KL:
+            mul_mat_q_case<GGML_TYPE_IQ2_KL>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ3_KS:
+            mul_mat_q_case<GGML_TYPE_IQ3_KS>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ4_KS:
+            mul_mat_q_case<GGML_TYPE_IQ4_KS>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ4_KS_R4:
+            mul_mat_q_case<GGML_TYPE_IQ4_KS_R4>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ4_KT:
+            mul_mat_q_case<GGML_TYPE_IQ4_KT>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ1_KT:
+            mul_mat_q_case<GGML_TYPE_IQ1_KT>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ2_KT:
+            mul_mat_q_case<GGML_TYPE_IQ2_KT>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ3_KT:
+            mul_mat_q_case<GGML_TYPE_IQ3_KT>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ5_KS:
+            mul_mat_q_case<GGML_TYPE_IQ5_KS>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ5_KS_R4:
+            mul_mat_q_case<GGML_TYPE_IQ5_KS_R4>(ctx, args, stream);
+            break;
+        case GGML_TYPE_IQ2_KS:
+            mul_mat_q_case<GGML_TYPE_IQ2_KS>(ctx, args, stream);
+            break;
         case GGML_TYPE_IQ2_K:
             mul_mat_q_case<GGML_TYPE_IQ2_K>(ctx, args, stream);
             break;
@@ -317,6 +350,17 @@ bool ggml_cuda_should_use_mmq(enum ggml_type type, int cc, int64_t ne11, int64_t
         case GGML_TYPE_IQ4_K:
         case GGML_TYPE_IQ5_K:
         case GGML_TYPE_IQ6_K:
+        case GGML_TYPE_IQ2_KL:
+        case GGML_TYPE_IQ3_KS:
+        case GGML_TYPE_IQ4_KS:
+        case GGML_TYPE_IQ4_KS_R4:
+        case GGML_TYPE_IQ5_KS:
+        case GGML_TYPE_IQ5_KS_R4:
+        case GGML_TYPE_IQ2_KS:
+        case GGML_TYPE_IQ1_KT:
+        case GGML_TYPE_IQ2_KT:
+        case GGML_TYPE_IQ3_KT:
+        case GGML_TYPE_IQ4_KT:
             mmq_supported = true;
             break;
         default:
