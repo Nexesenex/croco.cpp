4c1f2c9acd05e173ad6d379a62ed968122a2d64a
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-08-08 01:24:10 +0200

    256 candidates (1024 with Grammar)

--- gpttype_adapter.cpp
+++ gpttype_adapter.cpp
@@ -1865,15 +1865,15 @@ const std::vector<samplers> & sampler_order, llama_grammar * grammar, float dyna
     bool use_grammar = grammar != nullptr;
     std::vector<llama_token_data> precache = (use_grammar ? std::vector<llama_token_data>(candidates) : std::vector<llama_token_data>(0));
 
-    sample_top_k(&candidates_p, 3000);
+    sample_top_k(&candidates_p, 256);
 
     if (use_grammar) {
         sample_grammar(file_format, n_vocab, &candidates_p, grammar);
-        // if top_k 3000 doesn't contain a valid candidate for this grammar, try again pre-cull
+        // if top_k 3000 (1024 for Croco) doesn't contain a valid candidate for this grammar, try again pre-cull
         if (candidates_p.size <= 0) {
             candidates_p = { precache.data(), precache.size(), false };
             sample_grammar(file_format, n_vocab, &candidates_p, grammar);
-            sample_top_k(&candidates_p, 3000);
+            sample_top_k(&candidates_p, 1024);
         }
     }
 
