a211f2415d854ad179d2420cf591f30d755303c6
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-08-08 23:26:45 +0200

    Force model version = 6

--- expose.h
+++ expose.h
@@ -56,7 +56,7 @@ struct load_model_inputs
     const int kcpp_main_gpu = 0;
     const char * vulkan_info = nullptr;
     const int blasbatchsize = 128;
-    const int forceversion = 0;
+    const int forceversion = 6; // DEFAULT 0
     const int gpulayers = 0;
     const float rope_freq_scale = 1.0f;
     const float rope_freq_base = 10000.0f;
