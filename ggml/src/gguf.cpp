9c1191541d46269e57f30b481a287500fc5baa45
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-08-08 23:23:38 +0200

    Drop the GGML count limitation limit

--- ggml/src/gguf.cpp
+++ ggml/src/gguf.cpp
@@ -693,9 +693,13 @@ struct gguf_context * gguf_init_from_file_ptr(FILE * file, struct gguf_init_para
             ok = ok && gr.read(info.t.type);
 
             // check that tensor type is within defined range
-            if (info.t.type < 0 || info.t.type >= GGML_TYPE_COUNT) {
-                GGML_LOG_ERROR("%s: tensor '%s' has invalid ggml type %d. should be in [0, %d)\n",
-                    __func__, info.t.name, info.t.type, GGML_TYPE_COUNT);
+            // if (info.t.type < 0 || info.t.type >= GGML_TYPE_COUNT) {
+                // GGML_LOG_ERROR("%s: tensor '%s' has invalid ggml type %d. should be in [0, %d)\n",
+                    // __func__, info.t.name, info.t.type, GGML_TYPE_COUNT);
+
+            if (info.t.type < 0) {
+                GGML_LOG_ERROR("%s: tensor '%s' has invalid ggml type %d (%s)\n",
+                    __func__, info.t.name, info.t.type, ggml_type_name(info.t.type));
                 ok = false;
                 break;
             }
