6409cd2e2ac87ca871a7cfc6f03ab398a7fc6bec
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-08-08 22:53:23 +0200

    Double the anti-abuse limits

--- koboldcpp.py
+++ koboldcpp.py
@@ -72,11 +72,11 @@ overridekv_max = 16
 default_autofit_padding = 1024
 lora_filenames_max = 4
 
-# abuse prevention
-stop_token_max = 256
-ban_token_max = 5000 # 768
-logit_bias_max = 512
-dry_seq_break_max = 128
+# abuse prevention (double of official KCPP)
+stop_token_max = 512 # 256
+ban_token_max = 8192 # 768
+logit_bias_max = 1024 # 512
+dry_seq_break_max = 256 # 128
 extra_images_max = 4 # for kontext/qwen img
 
 # global vars
