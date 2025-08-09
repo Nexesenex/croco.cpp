13f9fadd22c8f6ecf51dab53b2c16fb004024630
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-08-09 14:58:32 +0200

    IQ_K quants first gen (2, 3, 4, 5, 6)

--- ggml/src/ggml-cuda/mmq.cu
+++ ggml/src/ggml-cuda/mmq.cu
@@ -2,6 +2,7 @@
 #include "mmq.cuh"
 #include "quantize.cuh"
 #include "mmid.cuh"
+#include "iqk_cuda_common.h"
 
 static void ggml_cuda_mul_mat_q_switch_type(ggml_backend_cuda_context & ctx, const mmq_args & args, cudaStream_t stream) {
     switch (args.type_x) {
