de2a6a91d06d83501107dc14b12082de6302d523
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-08-10 00:26:29 +0200

    Disable I2_K cpu quantization.
    To allow compilation.

--- ggml/src/ggml-quants.h
+++ ggml/src/ggml-quants.h
@@ -40,8 +40,8 @@ GGML_API void quantize_row_iq4_xs_ref (const float * GGML_RESTRICT x, block_iq4_
 GGML_API void quantize_row_iq3_s_ref  (const float * GGML_RESTRICT x, block_iq3_s   * GGML_RESTRICT y, int64_t k);
 GGML_API void quantize_row_iq2_s_ref  (const float * GGML_RESTRICT x, block_iq2_s   * GGML_RESTRICT y, int64_t k);
 
-GGML_API void quantize_row_iq2_k_ref  (const float * GGML_RESTRICT x, block_iq2_k  * GGML_RESTRICT y, int64_t k);
-GGML_API void quantize_row_iq3_k_ref  (const float * GGML_RESTRICT x, block_iq3_k * GGML_RESTRICT y, int64_t k);
+// GGML_API void quantize_row_iq2_k_ref  (const float * GGML_RESTRICT x, block_iq2_k  * GGML_RESTRICT y, int64_t k);
+GGML_API void quantize_row_iq3_k_ref  (const float * GGML_RESTRICT x, block_iq3_k  * GGML_RESTRICT y, int64_t k);
 GGML_API void quantize_row_iq4_k_ref  (const float * GGML_RESTRICT x, block_iq4_k   * GGML_RESTRICT y, int64_t k);
 GGML_API void quantize_row_iq5_k_ref  (const float * GGML_RESTRICT x, block_iq5_k   * GGML_RESTRICT y, int64_t k);
 GGML_API void quantize_row_iq6_k_ref  (const float * GGML_RESTRICT x, block_iq6_k   * GGML_RESTRICT y, int64_t k);
@@ -135,7 +135,7 @@ GGML_API size_t quantize_q5_1(const float * GGML_RESTRICT src, void * GGML_RESTR
 GGML_API size_t quantize_q6_0(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
 GGML_API size_t quantize_q8_0(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
 
-GGML_API size_t quantize_iq2_k(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
+// GGML_API size_t quantize_iq2_k(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
 GGML_API size_t quantize_iq3_k(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
 GGML_API size_t quantize_iq4_k(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
 GGML_API size_t quantize_iq5_k(const float * GGML_RESTRICT src, void * GGML_RESTRICT dst, int64_t nrows, int64_t n_per_row, const float * imatrix);
