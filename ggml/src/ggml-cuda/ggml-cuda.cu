d20a1841685aa1b1ed1c819b3f06d34d29f49e52
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-09-01 23:20:18 +0200

    Update ggml-cuda.cu

--- ggml/src/ggml-cuda/ggml-cuda.cu
+++ ggml/src/ggml-cuda/ggml-cuda.cu
@@ -3487,6 +3487,7 @@ static bool ggml_backend_cuda_device_supports_op(ggml_backend_dev_t dev, const g
                     case GGML_TYPE_Q5_1:
                     case GGML_TYPE_Q6_0:
                     case GGML_TYPE_Q8_0:
+                    case GGML_TYPE_IQ4_NL:
                         return true;
                     default:
                         return false;
