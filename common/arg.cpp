8450a9aa1e005c41126b0155bd0eeb276e38d5ec
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-10-06 21:29:45 +0200

    Revert "llama : remove KV cache defragmentation logic (#15473)"

--- common/arg.cpp
+++ common/arg.cpp
@@ -2256,11 +2256,9 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
     ).set_examples({LLAMA_EXAMPLE_PERPLEXITY}));
     add_opt(common_arg(
         {"-dt", "--defrag-thold"}, "N",
-        string_format("KV cache defragmentation threshold (DEPRECATED)"),
+        string_format("KV cache defragmentation threshold (default: %.1f, < 0 - disabled)", (double)params.defrag_thold),
         [](common_params & params, const std::string & value) {
-            GGML_UNUSED(params);
-            GGML_UNUSED(value);
-            LOG_WRN("DEPRECATED: --defrag-thold is deprecated and no longer necessary to specify\n");
+            params.defrag_thold = std::stof(value);
         }
     ).set_env("LLAMA_ARG_DEFRAG_THOLD"));
     add_opt(common_arg(
--- src/llama-cparams.h
+++ src/llama-cparams.h
@@ -24,6 +24,7 @@ struct llama_cparams {
     float yarn_attn_factor;
     float yarn_beta_fast;
     float yarn_beta_slow;
+    float defrag_thold;
 
     bool embeddings;
     bool causal_attn;
