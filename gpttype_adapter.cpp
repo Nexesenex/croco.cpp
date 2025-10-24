51385e433ba82a98e39f281777e6ab10a094bb24
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-10-24 15:17:20 +0200

    Revert "rename blasbatchsize to batchsize"
    This reverts commit 57e1d9c8220acec6213833a2838707a89809f183.

--- gpttype_adapter.cpp
+++ gpttype_adapter.cpp
@@ -2154,7 +2154,7 @@ ModelLoadResult gpttype_load_model(const load_model_inputs inputs, FileFormat in
     kcpp_data->n_blasthreads = inputs.blasthreads;
     bool isGguf = (file_format == FileFormat::GGUF_GENERIC);
     kcpp_pipeline_parallelism = inputs.pipelineparallel;
-    kcpp_data->n_batch = GetBatchSize(inputs.batchsize, in_file_format);
+    kcpp_data->n_batch = GetBatchSize(inputs.blasbatchsize, in_file_format);
     kcpp_data->n_ubatch = kcpp_data->n_batch;
     if(isGguf && kcpp_pipeline_parallelism)
     {
