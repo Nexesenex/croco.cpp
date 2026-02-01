bb9ed3ed23963a2f141a60def4f2fe45d678ca14
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-02-01 15:27:27 +0100

    v1.107.2.10_b7937-5-4_Q6-IQ23456K_RMv1.21.99m

--- koboldcpp.py
+++ koboldcpp.py
@@ -72,12 +72,12 @@ dry_seq_break_max = 256
 extra_images_max = 8 # for kontext/qwen img
 
 # global vars
-KcppVersion = "1.107.2"
-LcppVersion = ""
-IKLcppVersion = ""
-EsoboldVersion = ""
-CudaSpecifics = ""
-ReleaseDate = ""
+KcppVersion = "1.107.2.10"
+LcppVersion = "b7937-5-4PR"
+IKLcppVersion = "Q6-IQ23456K"
+EsoboldVersion = "RMv1.21.99m"
+CudaSpecifics = "Cu128_Ar86_SMC2_DmmvX32Y1"
+ReleaseDate = "2026/02/04"
 showdebug = True
 kcpp_instance = None #global running instance
 global_memory = {"tunnel_url": "", "restart_target":"", "input_to_exit":False, "load_complete":False, "restart_model": "", "currentConfig": None, "modelOverride": None, "currentModel": None}
