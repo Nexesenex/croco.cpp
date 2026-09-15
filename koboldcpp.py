7e0eb2dc4a23f1a0fc42a7660ad43be7a4b9e759
Concedo <39025047+LostRuins@users.noreply.github.com>
2026-09-15 18:34:42 +0800

    bump video frame limit to 240/480

--- koboldcpp.py
+++ koboldcpp.py
@@ -3025,7 +3025,7 @@ def sd_generate(genparams):
     if flow_shift is not None and flow_shift < 0:
         flow_shift = None # fall back to the default
     sample_steps = (1 if sample_steps < 1 else (forced_steplimit if sample_steps > forced_steplimit else sample_steps))
-    vid_req_frames = (1 if vid_req_frames < 1 else (400 if vid_req_frames > 400 else vid_req_frames))
+    vid_req_frames = (1 if vid_req_frames < 1 else (480 if vid_req_frames > 480 else vid_req_frames))
     vid_fps = (16 if vid_fps < 16 else (32 if vid_fps > 32 else vid_fps))
 
     swap_refimg = (True if tryparseint(genparams.get("send_as_refimg", 0),0) else False)
